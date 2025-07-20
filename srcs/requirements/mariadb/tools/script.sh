8ead4bb3f219118bd130809f5565c5fd5fb934d3
lolqrdc <lola-rdc@outlook.fr>
2025-07-20 16:56:15 +0200

    script mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -e
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+if [ ! -f "/var/lib/mysql/.init_done" ]; then
     echo "==> Initializing MariaDB data directory..."
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     echo "==> Running SQL initialization script..."
     envsubst < /tools/init.sql | mysqld --user=mysql --bootstrap
+    
+    echo "==> Creating init marker..."
+    touch /var/lib/mysql/.init_done
     echo "==> MariaDB initial setup complete."
 fi
 

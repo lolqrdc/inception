34db3f5bf74d3c0b2c636cc05769ca838b66b06c
lolqrdc <lola-rdc@outlook.fr>
2025-07-20 17:03:10 +0200

    retour aux sources lol

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 set -e
 
-# Vérifier si MariaDB est déjà complètement configuré
-if [ ! -f "/var/lib/mysql/.init_done" ]; then
-    echo "==> Removing any existing data..."
-    rm -rf /var/lib/mysql/*
-    
+if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "==> Initializing MariaDB data directory..."
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     echo "==> Running SQL initialization script..."
     envsubst < /tools/init.sql | mysqld --user=mysql --bootstrap
-    
-    echo "==> Creating init marker..."
-    touch /var/lib/mysql/.init_done
     echo "==> MariaDB initial setup complete."
-else
-    echo "==> MariaDB already initialized."
 fi
 
 echo "==> Starting MariaDB server..."

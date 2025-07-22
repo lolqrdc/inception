04b32330145f2104ae52476bd44b6600481b829b
lolqrdc <lola-rdc@outlook.fr>
2025-07-22 10:42:27 +0200

    mon wp-config.php

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,44 +1,34 @@
 <?php
-// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 
-define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
-define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
-define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
-define( 'DB_HOST', 'mariadb' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+// ** Configuration de la base de données MySQL
+define('DB_NAME', getenv('MARIADB_DATABASE'));
+define('DB_USER', getenv('MARIADB_USER'));
+define('DB_PASSWORD', getenv('MARIADB_PASSWORD'));
+define('DB_HOST', 'mariadb');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
 
-/**#@+
- * Clés uniques d'authentification et salage.
- */
-define('AUTH_KEY',         'mettez une phrase unique ici');
-define('SECURE_AUTH_KEY',  'mettez une phrase unique ici');
-define('LOGGED_IN_KEY',    'mettez une phrase unique ici');
-define('NONCE_KEY',        'mettez une phrase unique ici');
-define('AUTH_SALT',        'mettez une phrase unique ici');
-define('SECURE_AUTH_SALT', 'mettez une phrase unique ici');
-define('LOGGED_IN_SALT',   'mettez une phrase unique ici');
-define('NONCE_SALT',       'mettez une phrase unique ici');
+// ** Clés uniques d'authentification et salage
+define('AUTH_KEY',         'mettre_une_phrase_unique_ici');
+define('SECURE_AUTH_KEY',  'mettre_une_phrase_unique_ici');
+define('LOGGED_IN_KEY',    'mettre_une_phrase_unique_ici');
+define('NONCE_KEY',        'mettre_une_phrase_unique_ici');
+define('AUTH_SALT',        'mettre_une_phrase_unique_ici');
+define('SECURE_AUTH_SALT', 'mettre_une_phrase_unique_ici');
+define('LOGGED_IN_SALT',   'mettre_une_phrase_unique_ici');
+define('NONCE_SALT',       'mettre_une_phrase_unique_ici');
 
-/**#@-*/
+// ** Préfixe des tables wordpress
+$table_prefix  = 'wp_';
 
-/**
- * Préfixe de base de données pour les tables de WordPress.
- */
-$table_prefix = 'wp_';
+// ** Pour le debug en dev
+define('WP_DEBUG', false);
 
-/**
- * Pour les développeurs : le mode débogage de WordPress.
- */
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', false );
+// ** Chemin absolu vers le dossier wordpress
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
 
-/* C'est tout, ne touchez pas à ce qui suit ! Bonne publication. */
+// ** Charge les fichiers de settings wordpress
+require_once(ABSPATH . 'wp-settings.php');
 
-/** Chemin absolu vers le dossier de WordPress. */
-if ( ! defined( 'ABSPATH' ) )
-    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 
-/** Réglage des variables de WordPress et de ses fichiers inclus. */
-require_once( ABSPATH . 'wp-settings.php' );
